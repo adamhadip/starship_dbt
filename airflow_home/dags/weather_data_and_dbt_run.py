a10f7f86873f37c800416763144616ccb7b761cb
Adam Hadi Pratama <adamhadip7@gmail.com>
2025-08-30 20:05:15 +0700

    save for a while

--- airflow_home/dags/weather_data_and_dbt_run.py
+++ airflow_home/dags/weather_data_and_dbt_run.py
@@ -6,6 +6,7 @@ This dag for running dbt run and dbt build for localhost and fetch api
 
 import pendulum
 import pandas as pd
+import requests
 from datetime import timedelta
 from airflow.decorators import dag
 from airflow.providers.standard.operators.bash import BashOperator
@@ -28,13 +29,15 @@ logging.basicConfig(level=logging.INFO)
 def project_data_analytics():
 
     @task(task_id="fetch_data_api")
-    
-    dbt_run = BashOperator(
-        task_id = 'dbt_running',
-        bash_command='cd /Users/adamhadipratama/starship_dbt/ && dbt run'
-    )
+    def fetch_api_data():
+        logging.info("Will be add if have monitor brok")
+        
+    # dbt_run = BashOperator(
+    #     task_id = 'dbt_running',
+    #     bash_command='cd /Users/adamhadipratama/starship_dbt/ && dbt run'
+    # )
 
-    hello_world >> dbt_run
-
+    # hello_world >> dbt_run
+    fetch_api_data
 project_data_analytics()
 
